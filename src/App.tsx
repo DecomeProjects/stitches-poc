b39de5521091f7ee09384dd2a72bd4ee75e7342a
Felipe Decome <decome.felipe@gmail.com>
2021-11-01 11:19:41 -0300

    feat: adding global styles to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-function App() {
-  return (
-    <h1>Olá mundo!</h1>
-  );
-}
+import React from 'react';
+import { globalStyle } from './styles/global';
+
+const App: React.FC = () => {
+  globalStyle();
+
+  return <h1>Olá mundo!</h1>;
+};
 
 export default App;
