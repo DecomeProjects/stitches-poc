d7f26adf085fdb9373a69347203cb3e0bb659975
Felipe Decome <decome.felipe@gmail.com>
2021-11-16 14:22:03 -0300

    chore: removing firebase service

--- src/services/firebase/app.ts
+++
@@ -1,5 +0,0 @@
-import { initializeApp, getApps, getApp } from 'firebase/app';
-
-import { firebaseConfig } from './config';
-
-export const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
