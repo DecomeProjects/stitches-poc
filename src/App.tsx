bd23b0c2d88c8b33fe5d1ea0e04ca6f4a45de11c
Felipe Decome <decome.felipe@gmail.com>
2021-11-16 08:39:59 -0300

    chore: adding contexts to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './services/firebase';
 
+import { AppProvider } from '@Hooks';
 import { Overview } from '@Pages/Overview';
 
 import { globalStyle } from './styles';
@@ -8,7 +9,11 @@ import { globalStyle } from './styles';
 const App: React.FC = () => {
   globalStyle();
 
-  return <Overview />;
+  return (
+    <AppProvider>
+      <Overview />
+    </AppProvider>
+  );
 };
 
 export default App;
